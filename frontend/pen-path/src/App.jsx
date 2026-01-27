509fdce72f4931de9292180627287f6e8a2127b2
Hadassah Roberts <118022490+HadsRobs19@users.noreply.github.com>
2026-01-27 13:29:48 -0500

    adding in fonts and polishing home page

--- frontend/pen-path/src/App.jsx
+++ frontend/pen-path/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
+import logo from './assets/logo.png';
 import './App.css'
 
-const BACKGROUND_GRADIENT = "linear-gradient(135deg, #667eea, #764ba2)";
+const BACKGROUND_GRADIENT = "linear-gradient(150deg, #B2F7FF, #98AEFD)";
 
 const App = () => {
 
@@ -13,14 +14,49 @@ const App = () => {
     padding: '0',
     display: 'flex',
     justifyContent: 'center',
-    alignItem: 'center',
+    alignItems: 'center',
     color: 'white',
     fontSize: '24px',
+    minHeight: '100%',
   };
+
+  const header = {
+    color: '#050505ff',
+    fontFamily: "'Play Kiddo'",
+    paddingBottom: "600px",
+    paddingLeft: "60px"
+  };
+
+  const logoFrame = {
+    width: "90%",
+    maxWidth: "375px",   
+    height: "667px",     
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingRight: '400px',
+    paddingBottom: '300px',
+};
+
+  const logoStyle = {
+    width: "auto",
+    maxWidth: "500px",
+    height: "auto",
+  };
+
 
   return (
     <div style={bgStyle}>
-      <h1>Pen Path</h1>
+      <div style={header}>
+        <h1>Welcome to</h1>
+      </div>
+      <div style={logoFrame}>
+        <img 
+          src={logo} 
+          alt="Pen Path logo" 
+          style={logoStyle}
+        />
+      </div>
     </div>
   )
 }
