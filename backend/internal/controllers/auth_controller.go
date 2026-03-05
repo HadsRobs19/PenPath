9309ddc69da00cbd476d954df95640ae58077abe
Hadassah Roberts <118022490+HadsRobs19@users.noreply.github.com>
2026-03-05 10:07:02 -0500

    added a scripts and docs folder

--- backend/internal/controllers/auth_controller.go
+++ backend/internal/controllers/auth_controller.go
@@ -1,1 +1,12 @@
 package controllers
+
+import "penpath-backend/internal/databases"
+
+type AuthController struct {
+	DB *databases.DBManager
+}
+
+// constructor
+func NewAuthController(db *databases.DBManager) *AuthController {
+	return &AuthController{DB: db}
+}
--- backend/internal/routes/auth_routes.go
+++ backend/internal/routes/auth_routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	backend "penpath-backend"
+	//"penpath-backend/internal/controllers"
+	"penpath-backend/internal/databases"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func RegisterUserRoute(app *fiber.App, db *databases.DBManager) {
+	//authController := controllers.NewAuthController(db)
+
+	//app.Get("/health", healthController.HealthCheck)
+	backend.PrintInfo("Successfully Registered Healthcheck API Route!")
+}
